0e1f004987a2a3ffdc73b2dd494d4ed32c732b46
Vladimir Revchuk <carter123@ua.fm>
2018-05-08 18:09:25 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Библиотека для работы с XPath запросами для xml
+# autor Revchuk V.V.
---
+++ src/XPathQuery.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Вова Ревчук
+ * Date: 08.05.2018
+ * Time: 17:56
+ */
+namespace XPathQueries;
+
+class XPathQuery
+{
+    public static function loadFile () {
+        echo "111";
+    }
+}
