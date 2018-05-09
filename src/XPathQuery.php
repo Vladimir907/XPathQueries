f2b19f4a5fa8c29bdef4e452531f2142527cab01
Vladimir Revchuk <carter123@ua.fm>
2018-05-09 13:00:53 +0300

    start of development library

--- src/XPathQuery.php
+++ src/XPathQuery.php
@@ -9,7 +9,46 @@ namespace XPathQueries;
 
 class XPathQuery
 {
-    public static function loadFile () {
-        echo "111";
+    /**
+     *
+     */
+    public $xml_url = null;
+
+    /**
+     *
+     */
+    private $xml_simple = null;
+
+    /**
+     *
+     */
+    private $xml_dom = null;
+
+    /**
+     *
+     */
+    public function __construct( $xml_url )
+    {
+        $this->xml_url = $xml_url;
+    }
+
+    /**
+     *
+     */
+    private function _init_xml_simple () {
+        if (!empty($this->xml_simple)) return true;
+        $this->xml_simple = @simplexml_load_file($this->xml_url);
+        if ($this->xml_simple === false) return false;
+        return true;
+    }
+
+    /**
+     *
+     */
+    public function getXml()
+    {
+        if ($this->_init_xml_simple()!=true) return false;
+        return $this->xml_simple;
+
     }
 }
